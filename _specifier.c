f54db7513b6f966aaefeb0889a7cf1a6e4c88a00
Lesuuda <lesuudaljamani@gmail.com>
2023-04-26 02:22:16 -0700

    Education is when you read the fine print. Experience is what you get if you don't

--- _specifier.c
+++ _specifier.c
@@ -10,10 +10,12 @@ int (*_specifier(const char *format))(va_list)
 {
 	int i;
 
-	func_t my_specifiers[4] = {
+	func_t my_specifiers[6] = {
 		{"c", print_char},
 		{"s", print_str},
 		{"%", print_percent},
+		{"d", print_decimal},
+		{"i", print_int},
 		{NULL, NULL}
 	};
 	for (i = 0; my_specifiers[i].t != NULL; i++)
